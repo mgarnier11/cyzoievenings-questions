6b7c75d2e1d8c87ef8430125c6ed6a1d2f49a491
mgarnier11 <mgarnier11@gmail.com>
2019-01-29 22:51:16 +0100

    added apk

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ const port = process.env.PORT || 8080;
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/apk', (req, res) => {
+    console.log('apk downloaded');
+    res.download(path.resolve(__dirname, './app-debug.apk'));
+});
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
     console.log('file sent');
     res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
+
 
 app.listen(port, () => {
     console.log(`listenning on port ${port}`);
